7a0fed15a798ec337963b5288be129ee2a1aefa4
Piotr Migaj <piotr.migaj@capgemini.com>
2023-04-25 21:20:35 +0200

    add frontend layouts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { HomeComponent } from './modules/home/home.component';
 import { ProductComponent } from './modules/product/product.component';
+import { FullpageComponent } from './layouts/fullpage/fullpage.component';
+import { LoginComponent } from './modules/login/login.component';
+import { FullpageadminComponent } from './layouts/fullpageadmin/fullpageadmin.component';
+import { AdminComponent } from './modules/admin/admin.component';
 
 const routes: Routes = [
   {
     path:'',component:DefaultComponent,children:[
       {path:'',component:HomeComponent},
       {path:'products',component:ProductComponent},
+    ]
+  },
+  {
+    path:'',component:FullpageComponent,children:[
+      {path:'login',component:LoginComponent},
+    ]
+  },
+  {
+    path:'',component:FullpageadminComponent,children:[
+      {path:'admin',component:AdminComponent},
     ]
   }
 ];
